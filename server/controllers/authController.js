2d96dac5e6f81c6e50914144e4d3bde1b07b37ad
yagisuke <yagisuke.0209@gmail.com>
2019-04-04 00:12:19 +0900

    signup-validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,27 @@
-exports.validateSignup = () => {};
+exports.validateSignup = (req, res, next) => {
+  req.sanitizeBody('name')
+  req.sanitizeBody('email')
+  req.sanitizeBody('password')
+
+  req.checkBody('name', 'Enter a name').notEmpty()
+  req.checkBody('name', 'Name must be between & and 10 characters')
+    .isLength({ min: 4, max: 10 })
+  
+  req.checkBody('email', 'Enter a valid email')
+    .isEmail()
+    .normalizeEmail()
+
+  req.checkBody('password', 'Enter a password').notEmpty()
+  req.checkBody('password', 'Password must be between & and 10 characters')
+    .isLength({ min: 4, max: 10 })
+
+  const errors = req.validationErrors()
+  if (errors) {
+    const firstErr = errors.map(err => err.msg)[0]
+    return res.status(400).send(firstErr)
+  }
+  next()
+}
 
 exports.signup = () => {};
 
