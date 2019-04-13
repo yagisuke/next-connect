282fdc844fb5b53be7118719b77772db790c9163
yagisuke <yagisuke.0209@gmail.com>
2019-04-13 19:28:47 +0900

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,37 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
-import withStyles from "@material-ui/core/styles/withStyles";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ShareOutlined from '@material-ui/icons/ShareOutlined'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+  const { user = {} } = auth || {}
+
+  return (
+    <AppBar
+      className={classes.appBar}
+      position={router.pathname === '/' ? 'fixed' : 'static' }>
+      <Toolbar>
+        <ShareOutlined className={classes.icon} />
+        <Typography variant="h5" component="h1" className={classes.toolbarTitle}>
+          NextConnect
+        </Typography>
+        {user._id ? (
+          <div>
+            <Button>Profile</Button>
+            <Button variant="outlined">Sign out</Button>
+          </div>
+        ) : (
+          <div>
+            <Button>Sign in</Button>
+            <Button>Sign up</Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 const styles = theme => ({
   appBar: {
@@ -20,6 +44,6 @@ const styles = theme => ({
   icon: {
     marginRight: theme.spacing.unit
   }
-});
+})
 
-export default withStyles(styles)(Navbar);
+export default withStyles(styles)(Navbar)
