fa5d62f5ad10ef1df02d94de800a281dc2698d77
yagisuke <yagisuke.0209@gmail.com>
2019-04-13 16:35:05 +0900

    like-unlike

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -58,6 +58,18 @@ exports.getPostFeed = async (req, res) => {
   res.json(posts)
 }
 
-exports.toggleLike = () => {}
+exports.toggleLike = async (req, res) => {
+  const { postId } = req.body
+  const post = await Post.findOne({ _id: postId })
+  const likeIds = post.likes.map(id => id.toString())
+  const authUserId = req.user._id.toString()
+  if (likeIds.includes(authUserId)) {
+    await post.likes.pull(authUserId)
+  } else {
+    await post.likes.push(authUserId)
+  }
+  await post.save()
+  res.json(post)
+}
 
 exports.toggleComment = () => {}
